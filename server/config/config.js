d334db320d921b2b5f4b09277a64b172549bd0a6
Brayan <brayansamsungfame@gmail.com>
2020-03-24 17:00:39 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://brayannode:SG5KfJvvpPab34pG@cluster0-5jeqx.mongodb.net/test';
+    // urlDB = 'mongodb+srv://brayannode:SG5KfJvvpPab34pG@cluster0-5jeqx.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
