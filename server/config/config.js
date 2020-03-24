91547b74b3c2b3e2e9be665020bd9d34d464fa4f
Brayan <brayansamsungfame@gmail.com>
2020-03-24 14:58:00 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,22 @@
 //==========================
 //      PUERTO
 //  ========================
+process.env.PORT = process.env.PORT || 3000;
 
-process.env.PORT = process.env.POR || 3000;
+
+//============================
+//          ENTORNO
+//============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//============================
+//          BD
+//============================
+let urlDB;
+if ( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://brayannode:SG5KfJvvpPab34pG@cluster0-5jeqx.mongodb.net/test';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,35 +14,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
  
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario')
-});
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
 
-    if (body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        })
-    }else{
-        res.json({
-            persona: body
-        })
-    }
-});
+// await mongoose.connect('mongodb://localhost:27017/cafe', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
+mongoose.connect(process.env.URLDB,
+                 {useNewUrlParser: true, useUnifiedTopology: true},
+                 (err, res) => {
+    if ( err ) throw err
+        console.log('Base de Datos ONLINE');
+                 }
+);
+mongoose.set('useCreateIndex', true);
 
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-});
+app.use( require('./routes/usuario') );
  
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto', 3000);
